ddce41cb61d2cd1204fa2894b3492c9d73a18b34
EddieEdTheEd <edward.huynh2@student.education.wa.edu.au>
2020-06-18 10:11:59 +0800

    because it broke(do not pull)
    do not pull this one, its work in progress

---
+++ ModelForAll.js
@@ -0,0 +1,5 @@
+const variableName = document.getElementsByClassName("hp").a = "<a>HP</a>: 63.64 // <a>76.85</a><br>" 
+
+
+console.log(variableName);
+
--- poke.js
+++ poke.js
@@ -242,9 +242,21 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+var modal1 = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+}
+
+btn1.onclick = function() {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
